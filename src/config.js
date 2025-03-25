41a71adfe2b4ad10e9aea15f58f2ca6a407a25fd
MiguelAngel-del <miguelgarcia@umes.edu.gt>
2025-03-24 21:13:18 -0600

    urlfrontedn agregado cambios realizados

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import { config } from 'dotenv'
 config();
 
 export default {
-    secretKey: process.env.SECRET_KEY || '',
+    //secretKey: process.env.SECRET_KEY || '',
     port: process.env.PORT || 4000,
     dbUser: process.env.DB_USER || '',
     dbPassword: process.env.DB_PASSWORD || '',
